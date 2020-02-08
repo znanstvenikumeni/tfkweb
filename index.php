eca6781baebc423e331aa7508a5238e2a8ee4203
Mario Borna Mjertan <mbm@educateam.hr>
2020-02-08 15:52:06 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="/frontend/main.css">
+
+    <title>Coming soon &middot; Together for Knowledge</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid primary-frontpage">
+  <div class="container">
+    <a href="/"><img src="tfk-logo-black.png" alt="TFK" class="logo invert"></a>
+    <h1 class="display-4 white">Coming soon.</h1>
+    <p class="lead white">We're still developing the Together for Knowledge website.</p>
+  </div>
+</div>
+    <div class="container">
+    <p class="lead">In the meantime, you can get to know our work by <a href="https://initiative.togetherforknowledge.eu">visiting the TFK Initiative website</a> or the <a href="https://znanstvenikumeni.org">Znanstvenik u meni</a> website.</p>
+    <h1 class="display-4">Contact us</h1>
+    <p>If you need to contact us, feel free to send an email to tfk@togetherforknowledge.eu or natjecanje@znanstvenikumeni.org</p>
+  </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
